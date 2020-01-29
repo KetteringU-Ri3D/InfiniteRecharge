72937af10eb97ecb3b9daa94f14dc0dbd4b54e47
Jesse Estes <jestes5111@gmail.com>
2020-01-29 09:03:35 -0500

    Fixed FiveBallRightAuto and re-added gradle-wrapper.jar

--- src/main/java/frc/robot/auto/FiveBallRightAuto.java
+++ src/main/java/frc/robot/auto/FiveBallRightAuto.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.commands.auton.TimedDrive;
 import frc.robot.commands.auton.TimedTurn;
 import frc.robot.commands.intake.TimedIntakeIn;
+import frc.robot.commands.intake.TimedIntakeOut;
 import frc.robot.commands.shooter.TimedShooterOut;
 
 public class FiveBallRightAuto extends CommandGroup {
@@ -26,27 +27,27 @@ public class FiveBallRightAuto extends CommandGroup {
     // Shoot 3 balls ~4 seconds then turns
     addParallel(new TimedShooterOut(4)); // orig 5
     addSequential(new TimedIntakeIn(3.3)); // orig 4
-    addSequential(new TimedTurn(0.75, 0.27)); // turn right
+    addSequential(new TimedTurn(0.7, 0.6)); // turn right
     // 4.5 seconds used
 
     // Must travel back ~8 feet then slow down
-    addSequential(new TimedDrive(-0.75, 2)); // drive to near ball
+    addSequential(new TimedDrive(-0.75, 0.5)); // drive to near ball
     // 6.5 sec
 
     // Intake the balls
     addParallel(new TimedIntakeIn(4));
-    addSequential(new TimedDrive(-0.5, 3)); // about correct distance
+    addSequential(new TimedDrive(-0.5, 3.55)); // about correct distance
     // 9.5 sec
 
     // Code before
     //addParallel(new TimedDrive(0.6, 2.5));
     //addSequential(new TimedIntakeIn(1));
     // Code after
-    addParallel(new TimeIntakeOut(1)); // Prevent ball from rolling out. Maybe increase
-    addSequential(new TimedDrive(0.75, 3.5)); // drive back to line
+    addParallel(new TimedIntakeOut(1)); // Prevent ball from rolling out. Maybe increase
+    addSequential(new TimedDrive(0.75, 1.85)); // drive back to line
     // 13 seconds used
     addParallel(new TimedShooterOut(5)); // Rev up shooter
-    addSequential(new TimedTurn(-0.75, 0.15)); // turn left toward goal. Check angle
+    addSequential(new TimedTurn(-0.7, 0.3)); // turn left toward goal. Check angle
 
     // Shoot final balls
     //addParallel(new TimedShooterOut(5));
